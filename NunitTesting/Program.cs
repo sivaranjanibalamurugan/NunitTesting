ba8f6486a6fe7e553d9af8496d499d945dca612b
unknown <sivaranjanib99@gmail.com>
2021-07-02 18:04:10 +0530

    [siva] calculating the monthly payament

--- NunitTesting/Program.cs
+++ NunitTesting/Program.cs
@@ -9,6 +9,7 @@ namespace NunitTesting
             Console.WriteLine("Hello World!");
             Console.WriteLine("1 - Day of week");
             Console.WriteLine("2 - Temperature Conversion");
+            Console.WriteLine("3 - Monthly Payment");
             Console.WriteLine("Enter you choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -20,16 +21,21 @@ namespace NunitTesting
                     DayofWeek.ReadInput();
                     break;
 
-
-
                 case 2:
                     TemperatureConversion temperatureConversion = new TemperatureConversion();
                     temperatureConversion.ReadInput();
                     break;
+                case 3:
 
+                    MonthlyPayment MonthlyPayment = new MonthlyPayment();
+                    MonthlyPayment.ReadInput();
+                    break;
 
-                    Console.WriteLine("3 - Temperature Conversion");
+                default:
+                    break;
+
             }
+            Console.Read();
         }
     }
 }
