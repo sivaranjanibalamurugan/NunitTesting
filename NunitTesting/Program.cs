360f1c8b79ce80171fe72d18eade47bfddb5191c
unknown <sivaranjanib99@gmail.com>
2021-07-02 16:43:41 +0530

    [siva] checking the day of the week

--- NunitTesting/Program.cs
+++ NunitTesting/Program.cs
@@ -6,7 +6,21 @@ namespace NunitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to N Unit Testing Programs"); 
+            Console.WriteLine("1) Day of week");
+            Console.WriteLine("Enter you choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    DayOfWeek dayOfWeek = new DayOfWeek();
+                    dayOfWeek.ReadInput();
+                    break;
+
+                default:
+                    break;
+            }
+            Console.Read();
         }
     }
 }
