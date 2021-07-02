785edc43f1ae11ab5bd1a251644c270bc53f6018
unknown <sivaranjanib99@gmail.com>
2021-07-02 17:38:59 +0530

    [siva] converting the temperature

--- NunitTesting/Program.cs
+++ NunitTesting/Program.cs
@@ -7,6 +7,29 @@ namespace NunitTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("1 - Day of week");
+            Console.WriteLine("2 - Temperature Conversion");
+            Console.WriteLine("Enter you choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+
+                case 1:
+
+                    DayofWeek DayofWeek = new DayofWeek();
+                    DayofWeek.ReadInput();
+                    break;
+
+
+
+                case 2:
+                    TemperatureConversion temperatureConversion = new TemperatureConversion();
+                    temperatureConversion.ReadInput();
+                    break;
+
+
+                    Console.WriteLine("3 - Temperature Conversion");
+            }
         }
     }
 }
